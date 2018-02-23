0b1203792064128c411845c8ecc3d930b13cedf3
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2018-02-24 00:24:11 +0900

    implement "-n" option

--- cat.go
+++ cat.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+)
+
+var (
+	lineNumberOpt = flag.Bool("n", false, "Number the output lines")
 )
 
 func main() {
+
 	flag.Parse()
-	//fmt.Println(flag.Arg(0))
+
 	file, err := os.Open(flag.Arg(0))
 	if err != nil {
 		fmt.Println("Could not open file", err)
@@ -17,17 +23,29 @@ func main() {
 	}
 	defer file.Close()
 
+	text := make([]byte, 0)
 	buf := make([]byte, 100)
+
 	for {
 		n, err := file.Read(buf[0:])
 		if err != nil {
-			if err == io.EOF {
+			if err == io.EOF { // deep nest
 				break
 			}
 			fmt.Println("Could not read file", err)
 			return
 		}
-		fmt.Print(string(buf[:n]))
+		text = append(text, buf[:n]...)
+	}
+
+	textArray := strings.Split(string(text), "\n")
+
+	if *lineNumberOpt {
+		for num, line := range textArray {
+			fmt.Printf("%4d  %s\n", num, line)
+		}
+	} else {
+		fmt.Print(string(text))
 	}
 
 }
