4e4a96322b9cb3a101eafa2b9bd7eef317710649
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2018-02-26 21:04:18 +0900

    add processing when argument is not directory

--- ls.go
+++ ls.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
@@ -16,15 +17,37 @@ func main() {
 		path = flag.Arg(0)
 	}
 
+	if !isExist(path) {
+		fmt.Println("No such file or directory")
+		return
+	}
+
+	if !isDir(path) {
+		fmt.Println(path)
+		return
+	}
+
 	files, err := ioutil.ReadDir(path)
 
 	if err != nil {
 		println(err)
+		return
 	}
 
 	for _, file := range files {
 		fmt.Printf("%s ", file.Name())
 	}
+
 	fmt.Println()
 
+}
+
+func isExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func isDir(path string) bool {
+	Info, _ := os.Stat(path)
+	return Info.IsDir()
 }
